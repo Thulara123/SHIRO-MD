dfcad52c48836ef462e3ac135906aed683ff6a39
Thulara123 <160054960+Thulara123@users.noreply.github.com>
2025-01-20 16:30:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG : process.env.ALIVE_IMG || "image url",
+ALIVE_IMG : process.env.ALIVE_IMG || "asset/SHIRO-MD.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "Hello,I am SHIRO-MD. I am alive now !!!"
 };
