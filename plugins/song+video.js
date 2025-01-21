2fdbc8826345aa26510f4075381be490da1b2cd1
Thulara123 <160054960+Thulara123@users.noreply.github.com>
2025-01-21 11:11:04 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -1,1 +1,44 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
 
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("Please give me a url or titile")
+const search = await yts(q)
+const data = search.video[0]:
+  const url = data.url
+
+  let desc= `
+  *SHIRO-MD SONG DOWNLOADER*
+  
+  titile : ${data.title}
+  description : ${data.description}
+  time : ${data.timestamp}
+  ago : ${data.ago}
+  views : ${data.views}
+  
+  MADE BY SHIRO-MD`
+
+await conn.sendMessage(form,{image:{url: data.thumnail},caption:desc},{quoted:mek});
+
+//download audio
+
+let down = await fg.yta(url)
+let downloadUrl - down.dl_url
+
+//send audio massage
+await conn.sendMessage(form,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quored:mek})
+
+  
+}catch(e){
+  console.log(e)
+  reply(`${e}`)
+}
+})
