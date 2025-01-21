08fb496a7cac1bff1e69bdc8297b082b88a1011b
Thulara123 <160054960+Thulara123@users.noreply.github.com>
2025-01-21 12:55:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "uMICUDYZ#YH5XoKjTVzGurLQ2q43e-7r-Yz6a2kSLX-LnbNwUvwo",
 ALIVE_IMG : process.env.ALIVE_IMG || "asset/SHIRO-MD.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "Hello,I am SHIRO-MD. I am alive now !!!",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true"
 };
