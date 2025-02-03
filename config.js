8b686e8fe9f0b78b02c443f797c6f47e68bcf99f
Thulara123 <160054960+Thulara123@users.noreply.github.com>
2025-02-03 08:10:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,4 @@ ALIVE_IMG : process.env.ALIVE_IMG || "asset/SHIRO-MD.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "Hello,I am SHIRO-MD. I am alive now !!!",
 AUTO_READ_STATUS : process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-DATABASE_URL: process.env.DATABASE_URL === undefined ? 'postgres://movie_my_user:BDXztL7cmv1gV26b9eCsAseSMp7tqyvW@dpg-co1n7jvsc6pc73ctrku0-a.oregon-postgres.render.com/movie_my' : process.env.DATABASE_URL,
-PREFIX: process.env.PREFIX || '.' ,
-JID: process.env.JID || `94741688630@s.whatsapp.net` , 
-FOOTER: process.env.FOOTER === undefined ? 'SHIRO-MD MOVIE DDOWMLOAD' : process.env.FOOTER
 };
