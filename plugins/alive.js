ceac2be71b6cafa837988dbdf06e87ba789ff1e8
Thulara123 <160054960+Thulara123@users.noreply.github.com>
2025-02-03 14:23:28 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -5,7 +5,6 @@ cmd({
     pattern: "alive",
     desc: "Check bot online or no.",
     category: "main",
-    react: "💚",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
