22a6119fece7bdc4d9422ff823dabd2b62cceca8
Thulara123 <160054960+Thulara123@users.noreply.github.com>
2025-02-03 21:52:25 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -18,8 +18,8 @@ const deta = search.videos[0];
 const url = deta.url 
 
 let desc= `
- 🎶*SHIRO-MD*   𝗔𝗨𝗗𝗜𝗢-𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗥🎶
-|__________
+ 🎶 *SHIRO-MD*   𝗔𝗨𝗗𝗜𝗢-𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗥🎶
+ __________
 | ℹ️ title : ${deta.title}
 | 📋 description : ${deta.description}
 | 🕘 time : ${deta.timestamp}
@@ -39,8 +39,8 @@ let down = await fg.yta(url)
 let downloadUrl = down.dl_url
 
 //send audio message 
-await conn.sendMessage(from,{audio:{url:downloadUrl},mimetype:"audio/mpeg",caption :"> ©SHIRO-MD"},{quoted:mek})
-await conn.sendMessage(from,{document:{url:downloadUrl},mimetype:"audio/mpeg",fileName:deta.title + ".mp3" ,caption :"> ©SHIRO-MD"},{quoted:mek})
+await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg",caption :"> ©SHIRO-MD"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:deta.title + ".mp3" ,caption :"> ©SHIRO-MD"},{quoted:mek})
 
   
 
@@ -68,9 +68,9 @@ const deta = search.videos[0];
 const url = deta.url 
 
 let desc= `
-📽️*SHIRO-MD*   𝗩𝗜𝗗𝗘𝗢-𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗥📽️
-|__________
-| ℹ️ title : ${deta.title}
+📽️ *SHIRO-MD*   𝗩𝗜𝗗𝗘𝗢-𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗥📽️
+ __________
+|ℹ️ title : ${deta.title}
 | 📋 description : ${deta.description}
 | 🕘 time : ${deta.timestamp}
 | 📌 ago : ${deta.ago}
@@ -89,8 +89,8 @@ let down = await fg.ytv(url)
 let downloadUrl = down.dl_url
 
 //send video  message 
-await conn.sendMessage(from,{video:{url:downloadUrl},mimetype:"video/mp4",caption :"> ©SHIRO-MD"},{quoted:mek})
-await conn.sendMessage(from,{document:{url:downloadUrl},mimetype:"video/mp4",fileName:deta.title + ".mp4",caption :"> ©SHIRO-MD"},{quoted:mek})
+await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4",caption :"> ©SHIRO-MD"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:deta.title + ".mp4",caption :"> ©SHIRO-MD"},{quoted:mek})
 
   
 
