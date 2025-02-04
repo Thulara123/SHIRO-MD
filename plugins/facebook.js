809247cc995df0137404f7f387fba62fef39b537
Thulara123 <160054960+Thulara123@users.noreply.github.com>
2025-02-04 17:15:32 +0530

    Update facebook.js

--- plugins/facebook.js
+++ plugins/facebook.js
@@ -40,7 +40,7 @@ try{
 
   await m.react('✅');
   let video = data.url;
-  let dev = '© 2024 Queen Anju FB Downloader | Download with ease, cherish forever.'
+  let dev = '© 2025 SHIRO-MD FB Downloader | Download with ease, cherish forever.'
   
   try {
     await conn.sendMessage(m.chat, { video: { url: video }, caption: dev, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m });
