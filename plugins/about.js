91e869234539cd01169f1b395cfa15c918a8cd15
Thulara123 <160054960+Thulara123@users.noreply.github.com>
2025-02-04 13:35:24 +0530

    Create about.js

---
+++ plugins/about.js
@@ -0,0 +1,26 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "about",
+    desc: "To get the bot informations.",
+    react: "ℹ️",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let about = ` HELLOW THEIR ${senderNumber} I AM SHIRO-MD WHATSAPP BOT 
+              CREATED BY *{C0De_Zero};*
+              
+              > *© SHIRO Whatsapp Bot*
+              > *ɢɪᴛʜᴜʙ :* github.com/Thulara123/SHIRO-MD
+              
+              THANKS FOR USING SHIRO-MD`
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption:about},{quoted: mek})
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
