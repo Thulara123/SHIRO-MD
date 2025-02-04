43c13ab32744163fc6c4b8a22a7cb8b333fd09c2
Thulara123 <160054960+Thulara123@users.noreply.github.com>
2025-02-04 08:33:34 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -4,15 +4,15 @@ const {sleep} = require('../lib/functions')
 
 cmd({
     pattern: "restart",
-    desc: "restart the bot SHIRO-MD",
+    desc: "restart the bot",
     category: "owner",
-    react: "♻️",
+    react: "🔄"
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const {exec} = require("child_process")
-reply("𝐑𝐄𝐒𝐓𝐀𝐑𝐓𝐈𝐍𝐆...♻️")
+reply("restarting...!!!")
 await sleep(1500)
 exec("pm2 restart all")
 }catch(e){
