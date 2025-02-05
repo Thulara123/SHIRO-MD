8809eb40dc50b2ed4cf974afc63a897a40cff1ea
Thulara123 <160054960+Thulara123@users.noreply.github.com>
2025-02-05 21:18:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
+const prefix = "."
 
 const ownerNumber = ['94741688630']
 
@@ -40,14 +41,6 @@ const port = process.env.PORT || 8000;
 
 async function connectToWA() {
 
-//connect mongodb
-const connectDB = require('./lib/mongodb')
-connectDB();
-        
-//=================================
-const {readEnv} = require('./lib/database')
-const config = await readEnv();
-const prefix = config.PREFIX
 //=====================================
 
 console.log("Connecting wa bot.....!🔄");
