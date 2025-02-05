41a6e47655e66022767a8abd1812a35d75d845cd
Thulara123 <160054960+Thulara123@users.noreply.github.com>
2025-02-05 22:26:07 +0530

    Update imagine.js

--- plugins/imagine.js
+++ plugins/imagine.js
@@ -4,22 +4,26 @@ const fs = require('fs');
 const config = require('../config')
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 
+
 cmd({
     pattern: "imagine",
     react: '🪩',
     category: "imagine",
-    desc: "desct",
+    desc: "Generate an image from a prompt",  // Corrected the description text here
     dontAddCommandList: true,
-    use: '.prodia  woman,hair cut collor red,full body,bokeh',
+    use: '.prodia woman,hair cut collor red,full body,bokeh',
     filename: __filename
 },
 async(conn, mek, m,{from, l, prefix, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-if(!q) return reply("*Please Enter word ex .ailogo3 car")
-let res = await fetchJson('https://hercai.onrender.com/prodia/text2image?prompt='+q)
-conn.sendMessage(from, { image: { url: res.url }, caption: 'text to your name'}, { quoted: mek })
-} catch (e) {
-reply("*I Can't Create That Logo* 😓")
-l(e)
-}
+    try {
+        if(!q) return reply("*Please Enter word ex .ailogo3 car");
+        
+        let res = await fetchJson('https://hercai.onrender.com/prodia/text2image?prompt=' + q);
+        
+        conn.sendMessage(from, { image: { url: res.url }, caption: 'text to your name' }, { quoted: mek });
+        
+    } catch (e) {
+        reply("*I Can't Create That Logo* 😓");
+        l(e);
+    }
 });
