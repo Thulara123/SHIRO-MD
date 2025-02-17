4c5dbb1ba3654501b81e718f42262aaea7cb09d1
Thulara123 <160054960+Thulara123@users.noreply.github.com>
2025-02-17 10:40:08 +0530

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import cheerio from "cheerio";
+
+class SnackVideo {
+    async download(url) {
+        try {
+            let res = await axios.get(url);
+            let $ = cheerio.load(res.data);
+            let json = JSON.parse($("#VideoObject").text().trim());
+
+            return json.contentUrl;
+        } catch (error) {
+            throw { msg: error.message };
+        }
+    }
+}
+
+const snackVideo = new SnackVideo();
+
+let handler = async (m, { conn, args }) => {
+    if (!args[0]) return m.reply("Mana link videonya?");
+
+    try {
+        let videoUrl = await snackVideo.download(args[0]);
+        conn.sendMessage(m.chat, { 
+            video: { url: videoUrl }
+        });
+    } catch (error) {
+        m.reply(error.msg);
+    }
+};
+
+handler.help = ["snack"];
+handler.command = ["snack"];
+handler.tags = ['downloader']
+
+export default handler;
