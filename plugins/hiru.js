37e2faaaf83c79b3496dbf3d0ff6c6d948fba129
Thulara123 <160054960+Thulara123@users.noreply.github.com>
2025-02-27 21:01:15 +0530

    Update hiru.js

--- plugins/hiru.js
+++ plugins/hiru.js
@@ -32,7 +32,7 @@ cmd({
             newsInfo += `✨ *Title*: ${title}\n\n`;
             newsInfo += `📑 *Description*:\n${text}\n\n`;
             newsInfo += `⛓️‍💥 *Url*: www.hirunews.lk\n\n`;
-            newsInfo += `> *© 𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝙱𝚢 SHIRO-〽️𝙳*\n\n> *SHIRO-MD*`;
+            newsInfo += `> *© 𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝙱𝚢 SHIRO-〽️𝙳*\n\n*By SHIRO-MD*`;
 
             if (image) {
                 await conn.sendMessage(m.chat, {
