a5433e6fdfdcca961752664f9462de8a1f97f52f
Thulara123 <160054960+Thulara123@users.noreply.github.com>
2025-03-10 20:58:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -87,6 +87,8 @@ if (!mek.message) return
 mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
 if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
 await conn.readMessages([mek.key])
+const mnyako = await jidNormalizedUser(conn.user.id)
+await conn.sendMessage(mek.key.remoteJid, { react: { key: mek.key, text: '👾'}}, { statusJidList: [mek.key.participant, mnyako] })
 }
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
